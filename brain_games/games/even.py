ddf167bcdbf0b066b515449983b8d57b369ca202
Alex <saha12345m@gmail.com>
2026-08-29 15:35:10 +0400

    Added game logic for game 'Brain even' working with the new game engine

---
+++ brain_games/games/even.py
@@ -0,0 +1,11 @@
+import random
+
+
+DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".'
+
+def game():
+    result = {}
+    number = random.randint(1, 100)
+    result["Question"] = number
+    result["Answer"] = "yes" if number % 2 == 0 else "no"
+    return result
