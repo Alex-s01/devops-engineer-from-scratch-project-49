e97bffa3e949ce26d5a4f62734dd61038de4f6b0
Alex <saha12345m@gmail.com>
2026-08-29 15:39:07 +0400

    The file 'brain_even.py' was converted into a launch script that does not contain logic.

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,30 +1,8 @@
-import prompt
-import random
-
+from brain_games.game_engine import engine
+from brain_games.games import even
 
 def main():
-    print("Welcome to the Brain Games!")
-    name = prompt.string("May I have your name? ").capitalize()
-    print(f"Hello, {name}!")
-    print('Answer "yes" if the number is even, otherwise answer "no".')
-    
-    counter = 0
-    
-    while True:
-        number = random.randint(1, 100)
-        even = "yes" if number % 2 == 0 else "no"
-        print(f"Question: {number}")
-        answer = prompt.string("Your answer: ").lower()
-        if answer == even:
-            print("Correct!")
-            counter += 1
-        else:
-            print(f"'{answer}' is wrong answer ;(. Correct answer was '{even}'.")
-            print(f"Let's try again, {name}!")
-            return
-        if counter == 3:
-            print(f"Congratulations, {name}!")
-            return
+    engine(even.game, even.DESCRIPTION)
 
 if __name__ == "__main__":
     main()
