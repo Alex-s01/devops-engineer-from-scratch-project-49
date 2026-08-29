bf22c6ae633e7a325261bb69930d665cfa62e1b2
Alex <saha12345m@gmail.com>
2026-08-29 15:31:44 +0400

    Added game engine containing common logic

---
+++ brain_games/game_engine.py
@@ -0,0 +1,23 @@
+import prompt
+
+
+def engine(game, rules: str):
+    print("Welcome to the Brain Games!")
+    name = prompt.string("May I have your name? ").capitalize()
+    print(f"Hello, {name}!")
+    print(rules)
+
+    for _ in range(3):
+        session_vars = game()
+        print(f"Question: {session_vars.get('Question')}")
+        answer = prompt.string("Your answer: ").lower()
+
+        if answer == session_vars.get("Answer"):
+            print("Correct!")
+        else:
+            print(f"'{answer}' is wrong answer ;(. Correct answer was '{session_vars.get('Answer')}'.")
+            print(f"Let's try again, {name}!")
+            return
+
+    print(f"Congratulations, {name}!")
+    return
