a5829de81fbf67dbb5b2130d5c0cac06fbe83a7d
Alex <saha12345m@gmail.com>
2026-09-01 17:19:37 +0400

    The game engine now accepts entire game modules. Dictionaries have been removed and replaced with variables.

--- brain_games/game_engine.py
+++ brain_games/game_engine.py
@@ -2,21 +2,21 @@ import prompt
 from brain_games.cli import welcome_user
 
 
-def engine(game, rules: str):
+def engine(game_module):
     name = welcome_user()
-    print(rules)
+    print(game_module.DESCRIPTION)
 
     for _ in range(3):
-        session_vars = game()
-        print(f"Question: {session_vars.get('Question')}")
-        answer = prompt.string("Your answer: ").lower()
+        question, right_answer = game_module.get_question_and_answer()
+        print(f"Question: {question}")
+        user_answer = prompt.string("Your answer: ").lower()
 
-        if answer == session_vars.get("Answer"):
+        if user_answer == right_answer:
             print("Correct!")
         else:
             print(
-                    f"'{answer}' is wrong answer ;(. "
-                    f"Correct answer was '{session_vars.get('Answer')}'."
+                    f"'{user_answer}' is wrong answer ;(. "
+                    f"Correct answer was '{right_answer}'."
                     )
             print(f"Let's try again, {name}!")
             return
